60d3a4d33c85979d3d654d91b6c7aad20a4fa971
Waleed Ahmed <wahmed@tenna.com>
2020-02-21 15:03:24 +0500

    Adding url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 
 import ListPost from "./components/ListPost.js";
 import ViewPost from "./components/ViewPost.js";
 import CreatePost from "./components/CreatePost.js";
-import store from'./store';
+import store from "./store";
 import "./App.css";
 
 class App extends React.Component {
@@ -13,11 +13,15 @@ class App extends React.Component {
     return (
       <Provider store={store}>
         <BrowserRouter>
-        <Route path="/" exact component={ListPost} />
-        <Route path="/view/:postId" component={ViewPost} />
-        <Route path="/add" component={CreatePost} />
-        <Route path="/edit/:postId" component={CreatePost} />
-       </BrowserRouter>
+          <Route
+            path="https://aliidrees7777.github.io/ReactBlogApp/"
+            exact
+            component={ListPost}
+          />
+          <Route path="/view/:postId" component={ViewPost} />
+          <Route path="/add" component={CreatePost} />
+          <Route path="/edit/:postId" component={CreatePost} />
+        </BrowserRouter>
       </Provider>
     );
   }
